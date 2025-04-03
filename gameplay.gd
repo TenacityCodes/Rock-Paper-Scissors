7ca521b70ad527637b1623ff993c71f7cb5bf73a
TenacityCodes <owen.gyuritcza@gmail.com>
2025-04-03 15:55:52 -0400

    Added Main Gameplay

--- gameplay.gd
+++ gameplay.gd
@@ -1,14 +1,87 @@
 extends Node2D
 
-func _on_area_2d_mouse_entered() -> void:
-	pass
+var in_rock_area: bool = false
+var in_paper_area: bool = false
+var in_scissors_area: bool = false
 
+var rock: int = 1
+var paper: int = 2
+var scissors: int = 3
 
-func _on_area_2d_mouse_exited() -> void:
-	pass # Replace with function body.
+var player_choice: int = 0
+var cpu_choice: int = 0
 
+func _on_rock_area_2d_mouse_entered() -> void:
+	in_rock_area = true
+	print("Rock area: ", in_rock_area)
+
+func _on_rock_area_2d_mouse_exited() -> void:
+	in_rock_area = false
+	print("Rock area: ", in_rock_area)
+
+func _on_paper_area_2d_mouse_entered() -> void:
+	in_paper_area = true
+	print("Paper area: ", in_paper_area)
+
+func _on_paper_area_2d_mouse_exited() -> void:
+	in_paper_area = false
+	print("Paper area: ", in_paper_area)
+
+func _on_scissors_area_2d_mouse_entered() -> void:
+	in_scissors_area = true
+	print("Scissors area: ", in_scissors_area)
+
+func _on_scissors_area_2d_mouse_exited() -> void:
+	in_scissors_area = false
+	print("Scissors area: ", in_scissors_area)
 
 func _input(event:InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.double_click:
-			print("Wow, a left mouse click")
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if (in_rock_area):
+			player_choice = 1 
+			print("You selected rock")
+		elif (in_paper_area):
+			player_choice = 2 
+			print("You selected paper")
+		elif (in_scissors_area):
+			player_choice = 3
+			print("You selected scissors")
+			
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("enter"):
+		cpu_selection()
+		determine_outcome()
+
+func cpu_selection():
+	cpu_choice = randi_range(1,3)
+	print("cpu choice was: ", cpu_choice)
+
+func determine_outcome():
+	var outcome: String = ""
+	print("THIS player_choice: ", player_choice)
+	print("THIS cpu choice was: ", cpu_choice)
+	
+	if (cpu_choice == player_choice):
+		outcome = "It's A Draw!"
+		
+	elif (player_choice == 1): # Player chooses rock
+		if (cpu_choice == 2): # CPU chooses paper
+			outcome = "You Lost."
+		if (cpu_choice == 3): # CPU chooses scissors
+			outcome = "You Win!"
+	
+	elif (player_choice == 2): # Player chooses paper
+		if (cpu_choice == 1): # CPU chooses rock
+			outcome = "You Win!"
+		if (cpu_choice == 3): # CPU chooses scissors
+			outcome = "You Lost."
+			
+	elif (player_choice == 3): # Player chooses scissors
+		if (cpu_choice == 1): # CPU chooses rock
+			outcome = "You Lost."
+		if (cpu_choice == 2): # CPU chooses paper
+			outcome = "You Win!"
+		
+	print("Outcome: ", outcome)
+		
+	
