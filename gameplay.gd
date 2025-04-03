bc8fda9dd5e4cc249ed72ccfa4537591c14468c2
TenacityCodes <owen.gyuritcza@gmail.com>
2025-04-03 17:29:55 -0400

    More Debugging

--- gameplay.gd
+++ gameplay.gd
@@ -19,7 +19,9 @@ var cpu_selection_basic_label: String = "CPU Chose: "
 
 var items_can_be_changed: bool
 
-@onready var anticipation_wait_timer: float = 1
+var reset_can_be_pressed: bool
+
+@onready var anticipation_wait_timer: float = 0.5
 
 var player_choice: int = 0
 var cpu_choice: int = 0
@@ -27,30 +29,25 @@ var cpu_choice: int = 0
 func _ready() -> void:
 	outcome_to_display.visible = false
 	items_can_be_changed = true
+	reset_can_be_pressed = true
 
 func _on_rock_area_2d_mouse_entered() -> void:
 	in_rock_area = true
-	print("Rock area: ", in_rock_area)
 
 func _on_rock_area_2d_mouse_exited() -> void:
 	in_rock_area = false
-	print("Rock area: ", in_rock_area)
 
 func _on_paper_area_2d_mouse_entered() -> void:
 	in_paper_area = true
-	print("Paper area: ", in_paper_area)
 
 func _on_paper_area_2d_mouse_exited() -> void:
 	in_paper_area = false
-	print("Paper area: ", in_paper_area)
 
 func _on_scissors_area_2d_mouse_entered() -> void:
 	in_scissors_area = true
-	print("Scissors area: ", in_scissors_area)
 
 func _on_scissors_area_2d_mouse_exited() -> void:
 	in_scissors_area = false
-	print("Scissors area: ", in_scissors_area)
 
 func _input(event:InputEvent) -> void:
 	if (items_can_be_changed):
@@ -68,18 +65,20 @@ func _input(event:InputEvent) -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("enter"):
 		if (items_can_be_changed):
+			reset_can_be_pressed = false
 			items_can_be_changed = false
 			cpu_select()
 			await get_tree().create_timer(anticipation_wait_timer).timeout
 			determine_outcome()
-		
-	if Input.is_action_just_pressed("reset"):
-		try_again()
+			reset_can_be_pressed = true
+	
+	if (reset_can_be_pressed):
+		if Input.is_action_just_pressed("reset"):
+			reset()
 
 func cpu_select():
 	cpu_choice = randi_range(1,3)
-	print("cpu choice was: ", cpu_choice)
-	
+
 	if (cpu_choice == 1):
 		cpu_selection_label.text = cpu_selection_basic_label + "Rock"
 	elif (cpu_choice == 2):
@@ -90,7 +89,10 @@ func cpu_select():
 func determine_outcome():
 	var outcome: String = ""
 	
-	if (cpu_choice == player_choice):
+	if (player_choice == 0):
+		outcome = "No Input Entered"
+	
+	elif (cpu_choice == player_choice):
 		outcome = "It's A Draw!"
 		
 	elif (player_choice == 1): # Player chooses rock
@@ -111,12 +113,12 @@ func determine_outcome():
 		if (cpu_choice == 2): # CPU chooses paper
 			outcome = "You Win!"
 	
-	print()
-	print("Outcome: ", outcome)
 	outcome_to_display.visible = true
 	outcome_label.text = outcome
 	
-func try_again():
+func reset():
+	player_choice = 0
+	cpu_choice = 0
 	items_can_be_changed = true
 	player_selection_label.text = player_selection_basic_label
 	cpu_selection_label.text = cpu_selection_basic_label
